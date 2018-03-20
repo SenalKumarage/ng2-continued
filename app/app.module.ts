b4d9b0a59c54b66ebb7f09ef6d842099a5e6435e
Senal Kumarage <senalkumarage@gmail.com>
2018-03-20 09:11:01 +0800

    Wrapping jQuery

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,13 +17,16 @@ import {
 } from './events/index'
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { TOASTER_TOKEN, Toastr } from './common/toastr.service'
-import { CollapsibleWellComponent } from './common/collapsible-well.component'
+import { JQ_TOKEN, 
+         TOASTER_TOKEN, 
+         Toastr,
+         CollapsibleWellComponent } from './common/index'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
 
 declare let toastr:Toastr;
+declare let jQuery:Object;
 
 @NgModule({
   imports: [
